4a8d0ea3e6befb124808bf2449da0f0cbb8bfacb
kimberly.coston <kimberly.coston@yahoo.com>
2025-11-25 12:18:53 -0500

    adding cors new site

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,9 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174"
+        "http://127.0.0.1:5174",
+        "https://crrt-clot-app.vercel.app",
+        "https://*.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
